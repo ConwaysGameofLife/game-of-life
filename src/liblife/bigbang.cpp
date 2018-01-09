24982cbfbc2dfce97a5833b1b530de0324157e29
xinhuang <xinhuang.abc@gmail.com>
2018-01-09 01:07:27 -0800

    move implementation from .h to .cpp

--- src/liblife/bigbang.cpp
+++ src/liblife/bigbang.cpp
@@ -1,9 +1,20 @@
 #include "bigbang.h"
 
 #include "cpuavxuniverse.h"
+#include "gameconfig.h"
+#include "random.h"
 
 std::unordered_map<std::string, BigBang::factory_t> BigBang::factories_;
 
-void BigBang::init() {
-  add<CpuAvxUniverse>();
+std::unique_ptr<IUniverse> BigBang::create(const std::string &name, int width,
+                                           int height) {
+  auto u = factories_[name](width, height);
+  for (int i = 0; i < width * height * GameConfig::BORN_RATE; ++i) {
+    int x = Random::next<int>(width - 1);
+    int y = Random::next<int>(height - 1);
+    u->add(x, y);
+  }
+  return u;
 }
+
+void BigBang::init() { add<CpuAvxUniverse>(); }
--- src/liblife/bigbang.h
+++ src/liblife/bigbang.h
@@ -6,23 +6,14 @@
 #include <unordered_map>
 
 #include "iuniverse.h"
-#include "gameconfig.h"
-#include "random.h"
 
 class BigBang {
 public:
   using factory_t = std::function<std::unique_ptr<IUniverse>(int, int)>;
 
+  static void init();
   static std::unique_ptr<IUniverse> create(const std::string &name, int width,
-                                           int height) {
-    auto u = factories_[name](width, height);
-    for (int i = 0; i < width * height * GameConfig::BORN_RATE; ++i) {
-      int x = Random::next<int>(width - 1);
-      int y = Random::next<int>(height - 1);
-      u->add(x, y);
-    }
-    return u;
-  }
+                                           int height);
 
   template <typename T> static void add() {
     const auto &name = T::name();
@@ -30,8 +21,6 @@ public:
       return std::unique_ptr<IUniverse>(new T(w, h));
     };
   }
-
-  static void init();
 
 private:
   static std::unordered_map<std::string, factory_t> factories_;
